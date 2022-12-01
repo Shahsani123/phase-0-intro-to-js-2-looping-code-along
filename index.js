089f9c481613c3b7a1bc7589f013ff2b6edae2ae
+ <+>
2022-11-30 20:14:33 -0500

    commit tree 2b5798c602a96d637e3a646e016a3aa9452eb337

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const newNames = []
+    for (let i = 0; i < names.length; i++) {
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+    }
+    return newNames;
+
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
